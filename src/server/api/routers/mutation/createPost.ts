cda0b33b45a0d448b885796b14a7de666d959957
formyauto75@gmail.com <formyauto75@gmail.com>
2023-04-07 02:57:26 +0400

    feauture: trpc mutation of post request

---
+++ src/server/api/routers/mutation/createPost.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { publicProcedure } from "../../trpc";
+import { alreadyPost } from "../../middleware/alreadyPost";
+import { TRPCError } from "@trpc/server";
+
+export const authUser = () => {
+  return publicProcedure
+    .use(alreadyPost)
+    .input(
+      z.object({
+        client_id: z.string(),
+        title: z.string(),
+        gif: z.string(),
+        description: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        await ctx.prisma.post.create({
+          data: {
+            gif: input.gif,
+            description: input.gif,
+            title: input.title,
+            userId: input.client_id,
+          },
+        });
+
+        return {
+          success: true,
+          message: "post created",
+        };
+      } catch {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+    });
+};
