2396e3bd7fb56e9c7d88865d872837d548508383
formyauto75@gmail.com <formyauto75@gmail.com>
2023-04-16 01:49:50 +0400

    fix: undefined object return

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -21,7 +21,7 @@ export const PostPage: NextPage<PostProps> = ({ post }) => {
         <title>{post?.title}</title>
         <meta property="og:title" content={post?.title} />
         <meta property="og:description" content={post?.description} />
-        <meta property="og:image" content={post.author.avatar} />
+        <meta property="og:image" content={post?.author?.avatar} />
         <meta name="theme-color" content="#0070f1" />
       </Head>
       <Article post={post} />
