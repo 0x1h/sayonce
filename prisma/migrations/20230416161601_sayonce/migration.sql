ed33357075dedc155b966413b20af94ac4718b8b
formyauto75@gmail.com <formyauto75@gmail.com>
2023-04-16 20:16:15 +0400

    fix: prisma migration

--- prisma/migrations/20230416161601_sayonce/migration.sql
+++ prisma/migrations/20230416161601_sayonce/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "avatar" TEXT NOT NULL,
-    "ip" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
